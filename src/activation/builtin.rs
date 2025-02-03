2628768ef8ecb8ddd4df4a2dfbc8bfe227664c93
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2025-02-03 18:18:54 +0000

    small doc changes to activation builtin

--- src/activation/builtin.rs
+++ src/activation/builtin.rs
@@ -1,9 +1,9 @@
-/// The sigmoid activation function.
+/// The sigmoid activation function. Scales all values nonlinearly in the range of 1 to -1.
 pub fn sigmoid(n: f32) -> f32 {
     1. / (1. + std::f32::consts::E.powf(-n))
 }
 
-/// The ReLU activation function.
+/// The ReLU activation function. Equal to `n.max(0)``
 pub fn relu(n: f32) -> f32 {
     n.max(0.)
 }
