805d0751bcd37889944cf6a28ddb0d39cfbd730c
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2026-02-20 13:49:16 +0000

    pass the option to mutate_weight output instead of unwrapping

--- src/neuralnet.rs
+++ src/neuralnet.rs
@@ -376,9 +376,9 @@ impl<const I: usize, const O: usize> NeuralNetwork<I, O> {
     }
 
     /// Mutates a connection's weight.
-    pub fn mutate_weight(&mut self, connection: Connection, amount: f32, rng: &mut impl Rng) {
+    pub fn mutate_weight(&mut self, connection: Connection, amount: f32, rng: &mut impl Rng) -> Option<f32> {
         let n = &mut self[connection.from];
-        n.mutate_weight(connection.to, amount, rng).unwrap();
+        n.mutate_weight(connection.to, amount, rng)
     }
 
     /// Get a random valid location within the network.
