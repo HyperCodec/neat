3d8f98aa746e7e8ce9257ab70bcff7b6973341d2
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2026-02-20 18:43:21 +0000

    remove redundant check

--- src/activation.rs
+++ src/activation.rs
@@ -87,7 +87,7 @@ impl ActivationRegistry {
         let acts = self.activations();
 
         acts.into_iter()
-            .filter(|a| a.scope != NeuronScope::NONE && a.scope.contains(scope))
+            .filter(|a| a.scope.contains(scope))
             .collect()
     }
 
